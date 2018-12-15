75af8d2720a1a30412d71cb936ff9aed3dbea037
Jeffrey McAteer <jeffrey.p.mcateer@gmail.com>
2018-12-15 10:00:53 -0500

    sick docopts for server port numbers

---
+++ src/main.rs
@@ -0,0 +1,98 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use] extern crate rocket;
+use rocket::config::{Config, Environment};
+
+extern crate ws;
+
+#[macro_use] extern crate serde_derive;
+extern crate docopt;
+use docopt::Docopt;
+
+use std::thread;
+
+mod routes;
+
+const VERSION: &'static str = env!("CARGO_PKG_VERSION");
+const USAGE: &'static str = r#"
+The swiss army knife of all scheduling.
+
+Usage:
+  schedmap <config_file>
+  schedmap --server [<config_file>] [--app-port=<port>] [--websocket-port=<port>]
+  schedmap (-h | --help)
+  schedmap --version
+
+Options:
+  -h --help                Show this screen.
+  --app-port=<port>        Port for web UI [default: 8000].
+  --websocket-port=<port>  Port for websocket streams [default: 8001].
+  --version                Show version.
+"#;
+
+#[derive(Debug, Deserialize, Clone)]
+struct Args {
+    arg_config_file: Option<String>,
+    
+    flag_server: bool,
+    flag_app_port: u16,
+    flag_websocket_port: u16,
+    
+    flag_version: bool,
+}
+
+fn main() {
+  let args: Args = Docopt::new(USAGE)
+                      .and_then(|d| d.deserialize())
+                      .unwrap_or_else(|e| e.exit());
+  if args.flag_version {
+    println!("schedmap version {}", VERSION);
+    return;
+  }
+  
+  if args.flag_server {
+    run_server(args);
+    return;
+  }
+  
+  match args.arg_config_file {
+    Some(arg_config_file) => {
+      println!("Reading business config from file {}...", arg_config_file);
+      // Meh todo what will this become?
+    }
+    None => {
+      println!("{}", USAGE);
+      return;
+    }
+  }
+  
+}
+
+fn run_server(args: Args) {
+  let ws_args = args.clone();
+  let websocket_handle = thread::spawn(move || {
+    ws::listen(format!("0.0.0.0:{}", ws_args.flag_websocket_port), |out| {
+      move |msg| {
+        out.send(msg)
+      }
+    }).expect("Error on websocket server");
+  });
+  let web_args = args.clone();
+  let webserver_handle = thread::spawn(move || {
+    let config = Config::build(Environment::Staging)
+                .address("0.0.0.0")
+                .port(web_args.flag_app_port)
+                .finalize().expect("Could not configure rocket");
+
+    let app = rocket::custom(config);
+    
+    app.mount("/", routes![
+      routes::index
+    ]).launch();
+    
+  });
+  
+  websocket_handle.join().expect("Error when websocket_handle exited");
+  webserver_handle.join().expect("Error when webserver_handle exited");
+}
+
---
+++ src/routes.rs
@@ -0,0 +1,8 @@
+
+#[get("/")]
+pub fn index() -> &'static str {
+  "Hello, world!"
+}
+
+
+
