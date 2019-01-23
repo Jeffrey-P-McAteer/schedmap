adc1a6b12ccd7fe0eafe5ac7813c3de3c6726991
Jeffrey McAteer <jeffrey.p.mcateer@gmail.com>
2019-01-22 19:49:26 -0500

    build script updates

--- docker-build.ps1
+++ docker-build.ps1
@@ -1,6 +1,9 @@
 
-docker build -t schedmap_build_container .
+# Run this to build a local copy of the container
+# docker build -t schedmap_build_container .
+# Run this to restore a saved .tar containing a build container
+# docker load -i c:/path/to/schedmap_container.tar
 
-docker run -ti -v ${pwd}:/src/ -p 8000:8000 schedmap_build_container cargo run
+docker run -ti -v ${pwd}:/src/ -p 8000:8000 -p 8001:8001 schedmap_build_container cargo run
 
 
